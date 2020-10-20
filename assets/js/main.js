18a423de48d675a334bebd84a82b08815f34189a
Ricardo Barros <1191202@isep.ipp.pt>
2020-10-20 04:15:07 +0100

    updated project

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).on('ready', function() {
+  $(".slick-slider").slick({
+      dots: true,
+      infinite: false,
+      speed: 300,
+      slidesToShow: 1,
+      center: true
+    });
+});
